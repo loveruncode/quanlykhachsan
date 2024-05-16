01a999b42b2af289e54c9c42e000bd4aa54eda8b
my <miketran1406@gmail.com>
2024-05-16 09:19:50 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use App\Http\Middleware\CheckLoginMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
@@ -12,24 +11,17 @@ Route::post('/login', [UserController::class, 'checklogin'])->name('checklogin')
 
 
 
+    
 
-
-
-Route::middleware([CheckLoginMiddleware::class])->group(function () {
-    Route::get('/', function () {
-        return view('layout.master');
-    });
-
-    Route::get('/dashboard', function () {
-        return view('dashboard.dashboard');
-    });
-
-    Route::get('/profile', function () {
-        return view('profile.profile');
-    });
-
-    Route::get('/userAll', [UserController::class, 'index'])->name('user');
-
-    Route::get('/register', [UserController::class, 'register'])->name('register');
-    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+Route::get('/dashboard', function () {
+    return view('dashboard.dashboard');
 });
+
+Route::get('/profile', function () {
+    return view('profile.profile');
+});
+
+Route::get('/userAll', [UserController::class, 'index'])->name('user');
+
+Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
