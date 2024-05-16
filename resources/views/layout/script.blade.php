3a5165ae55050801d49050e76c08db7385b78927
my <miketran1406@gmail.com>
2024-05-16 11:24:54 +0700

    chance sweetalert

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -15,7 +15,7 @@
 <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
 <script src="{{ asset('js/setup.js') }}"></script>
 <script src="{{ asset('js/jquery-3.6.4.min.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="{{asset('js/sweetalert2@11.js')}}"></script>
 
 
 
