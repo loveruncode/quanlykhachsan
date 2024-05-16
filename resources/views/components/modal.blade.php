694a1bd60802f7db9974fa282d19c5cb089ee43d
my <miketran1406@gmail.com>
2024-05-16 11:13:26 +0700

    nhat my update to main

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -15,8 +15,11 @@
             </div>
             <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
             <div class="modal-footer">
+                <form action="{{route('logout')}}" method="get">
+                    @csrf
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="{{ url('/login') }}">Logout</a>
+                <button type="submit" class="btn btn-primary">Logout</button>
+            </form>
             </div>
         </div>
     </div>
