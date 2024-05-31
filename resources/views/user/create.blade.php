cdffc35c8e5e2c5fcc9e127122d15351958e0a74
Loveruncode <miketran1406@gmail.com>
2024-05-31 20:27:37 +0700

    update

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -5,25 +5,5 @@
 
     them thanh vien
 
-<script>
-    @if (session('success'))
-        Swal.fire({
-            icon: 'success',
-            title: 'Thành công!',
-            text: '{{ session('success') }}',
-        });
-    @endif
-</script>
-
-<script>
-    @if (session('error'))
-        Swal.fire({
-            icon: 'error',
-            title: 'Thất Bại!',
-            text: '{{ session('error') }}',
-        });
-    @endif
-</script>
-
 
 @endsection
