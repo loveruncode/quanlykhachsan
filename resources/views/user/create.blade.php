aa9c319f1f06f2e25e338279636e2f42bbf569d6
KhiemPham <khiempham161007@gmail.com>
2024-05-31 22:29:50 +0700

    feat(user): create add user form

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -2,8 +2,55 @@
 
 
 @section('content')
+<div class="page-header d-print-none">
+    <div class="container-fluid-xl">
+        <div class="row g-2 align-items-center">
+            <div class="col">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{route('dashboard')}}"
+                                class="text-muted">{{ __('Dashboard') }}</a></li>
+                        <li class="breadcrumb-item"><a href="{{route('notify.index')}}"
+                                class="text-muted">{{ __('Danh Sách Thành Viên') }}</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ __('Thêm Thành Viên') }}</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="page-body">
+    <div class="container-xl">
+        <form action="{{route('notify.store')}}" method="post">
+            @csrf
+            <div class="row justify-content-center">
+                @include('user.form.create-left')
+                @include('user.form.create-right')
+            </div>
+        </form>
+    </div>
+</div>
 
-    them thanh vien
+<script>
+@if(session('success'))
+Swal.fire({
+    icon: 'success',
+    title: 'Thành công!',
+    text: '{{ session('
+    success ') }}',
+});
+@endif
+</script>
 
+<script>
+@if(session('error'))
+Swal.fire({
+    icon: 'error',
+    title: 'Thất Bại!',
+    text: '{{ session('
+    error ') }}',
+});
+@endif
+</script>
 
 @endsection
