2e928feadcaf169880be2ce4dd4d219a312c8864
KhiemPham <khiempham161007@gmail.com>
2024-06-01 21:49:09 +0700

    fix create-left.blade.php &create-right.blade.php | update index.blade.php

--- resources/views/user/form/create-right.blade.php
+++ resources/views/user/form/create-right.blade.php
@@ -1,20 +1,19 @@
-<div class="col-12 col-md-4  ">
+<div class="col-12 col-md-4 col-sm-8">
     <div class="card mb-3 mt-2">
-        <div class=" card-header col-form-label">
+        <div class="card-header col-form-label">
             @lang('Hành Động')
         </div>
         <div class="d-flex">
             <div class="card-body">
-                <div class=" align-items-center h-100 gap-2">
-                    <button type="submit" class="btn btn-primary ">Thêm Thành Viên</button>
+                <div class="align-items-center h-100 gap-2">
+                    <button type="submit" class="btn btn-primary">Thêm Thành Viên</button>
                 </div>
             </div>
             <div class="card-body">
-                <div class=" align-items-center h-100 gap-2">
-                    <a href="{{route('user.index')}}" class="btn btn-primary ">Danh sách</a>
+                <div class="align-items-center h-100 gap-2">
+                    <a href="{{ route('user.index') }}" class="btn btn-primary">Danh sách</a>
                 </div>
             </div>
         </div>
-
     </div>
 </div>
