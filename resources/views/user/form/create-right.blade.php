8b04d5a76c4bfa7ef421cd4fe3a483ae0a0ab8f4
Loveruncode <miketran1406@gmail.com>
2024-06-04 23:53:08 +0700

    image for users

--- resources/views/user/form/create-right.blade.php
+++ resources/views/user/form/create-right.blade.php
@@ -16,4 +16,42 @@
             </div>
         </div>
     </div>
+    <div class="card mb-3 mt-2">
+        <div class="card-header col-form-label">
+         @lang('Thêm Ảnh Đại Diện')
+     </div>
+     <div class="card-body d-flex flex-column flex-sm-row align-items-center justify-content-between">
+         <input type="file" name="avatar" id="images" multiple accept="image/jpeg,image/png"
+             onchange="previewImages(event)">
+     </div>
+     <div class="card-body">
+         <div id="image-preview" class="mt-2"></div>
+     </div>
+ </div>
 </div>
+
+<script>
+    function previewImages(event) {
+        const preview = document.getElementById('image-preview');
+        preview.innerHTML = '';
+
+        const files = event.target.files;
+        for (const file of files) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const image = new Image();
+                image.src = e.target.result;
+                image.style.maxWidth = '100px';
+                image.style.marginRight = '10px';
+                preview.appendChild(image);
+            };
+
+            // Kiểm tra định dạng file trước khi đọc
+            if (file.type === 'image/jpeg' || file.type === 'image/png') {
+                reader.readAsDataURL(file);
+            } else {
+                alert('Chỉ chấp nhận file ảnh định dạng JPEG hoặc PNG.');
+            }
+        }
+    }
+    </script>
