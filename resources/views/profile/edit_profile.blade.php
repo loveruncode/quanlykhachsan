f843dd9be6a32de067ed883f28690831ae130232
NguyenTinh <nguyenductinh070@gmail.com>
2024-06-06 10:12:44 +0700

    update(profile): Profile editing page

--- resources/views/profile/edit_profile.blade.php
+++ resources/views/profile/edit_profile.blade.php
@@ -1,37 +1,37 @@
 <div class="card">
     <div class="card-header bg-primary text-white">
         <h3>Thông tin cá nhân</h3>
-        <img class="card-img-right rounded-circle float-end" id ="avatar" src="" alt="Avatar" width="150px" height="150px">
+        <img class="card-img-right rounded-circle float-end" id ="avatar" src="" alt="Avatar" width="120px" height="120px">
         <input type="file" id= "fileInput" onchange="previewAvatar()">
     </div>
     <div class="card-body">
     <table class="table">
       <tr>
         <td>Tên</td>
-        <td><input type="text" placeholder="ví  dụ: Nguyễn Văn A"></td>
+        <td><input type="text" value="Nguyễn Văn A" class='form-control'></td>
       </tr>
       <tr>
         <td>Email</td>
-        <td><input type="email" placeholder="nguyenvana@gmail.com"></td>
+        <td><input type="email" value="nguyenvana@gmail.com"  class='form-control'></td>
       </tr>
       <tr>
         <td>Số điện thoại</td>
-        <td><input type="number" placeholder="xxxxxxxxxx"></td>
+        <td><input type="number" placeholder="xxxxxxxxxx"  class='form-control'></td>
       </tr>
       <tr>
         <td>Ngày sinh</td>
         <td>
-          <input type="date" value="2024-06-02"/>
+          <input type="date"  class='form-control'/>
         </td>
       </tr>
       <tr>
         <td>Quốc tịch</td>
-        <td><input type="text" placeholder="Việt Nam"></td>
+        <td><input type="text" placeholder="Việt Nam"  class='form-control'></td>
       </tr>
       <tr>
         <td>Giới Tính</td>
         <td>
-            <select name="" id="">
+            <select name="" id=""  class='form-control'>
               <option value="nam">Nam</option>
               <option value="nữ">Nữ</option>
             </select>
@@ -39,7 +39,16 @@
       </tr>
       <tr>
         <td>Địa chỉ</td>
-        <td><input type="text" placeholder="Việt Nam"></td>
+        <td><input type="text" placeholder="Việt Nam"  class='form-control'></td>
+      </tr>
+      <tr>
+        <td>Đặt lại mật khẩu</td>
+        <td>
+          <label for="" class='d-flex flex-column'>Mật khẩu cũ</label>
+          <input type="password"  class='form-control'>
+          <label for="" class='d-flex flex-column'>Mật khẩu mới</label>
+          <input type="password"  class='form-control'>
+        </td>
       </tr>
     </table>
     <a href="#" class="btn btn-primary">Save</a>
