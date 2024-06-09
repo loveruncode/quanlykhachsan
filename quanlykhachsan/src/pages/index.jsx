d4a3e7ee9ed38a1356ece3a34e13b4d281ddb20d
KhiemPham <khiempham161007@gmail.com>
2024-06-09 13:12:05 +0700

    update client

---
+++ quanlykhachsan/src/pages/index.jsx
@@ -0,0 +1,8 @@
+import Home from './Home';
+import Profile from './Profile';
+import Contact from './Contact';
+import Shop from './Shop';
+import Room from './Room';
+import Blog from './Blog';
+
+export { Home, Profile, Contact, Shop, Room, Blog };
